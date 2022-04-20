7fe0ff5d45704527e7f009fd5e254a7ffac32682
farsenfsd <farsenfsd@gmail.com>
2022-04-20 20:16:46 +0100

    Versão inicial dos ficheiros servidor-cliente
    Versão bastante inicial

---
+++ src/src/main/java/pt/ubi/sd/g16/client/ClientInterface.java
@@ -0,0 +1,4 @@
+package pt.ubi.sd.g16.client;
+
+public interface ClientInterface extends java.rmi.Remote{
+}
---
+++ src/src/main/java/pt/ubi/sd/g16/server/ServerInterface.java
@@ -0,0 +1,4 @@
+package pt.ubi.sd.g16.server;
+
+public interface ServerInterface extends java.rmi.Remote{
+}
